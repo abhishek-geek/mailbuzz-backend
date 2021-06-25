53ad581373c1d38e57dc1e6817e0620a8c919ea3
abhishek-geek <2019005@iiitdmj.ac.in>
2021-06-25 18:15:25 +0530

    connected to mondo-db

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-// const { MONGODB_URI, PORT } = require("./utils/config");
+const { MONGODB_URI, PORT } = require("./utils/config");
 
 // connection to the MongoDB server
+
+mongoose.connect(
+    MONGODB_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    },
+    () => {
+      console.log(`connected to mongodb on port ${PORT}`);
+    }
+  );
 
 app.use(cors());
 app.use(express.json());
--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,9 @@
-const PORT = process.env.PORT || 3001;
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3002;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 module.exports = {
-    PORT
+    PORT,
+    MONGODB_URI
 }
