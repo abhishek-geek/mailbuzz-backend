22b16d3d5691d5d9b1681fdebc6e6f5f82bd83da
abhishek-geek <2019005@iiitdmj.ac.in>
2021-06-26 19:24:36 +0530

    editing future mail is onn

--- router/mails.js
+++ router/mails.js
@@ -14,6 +14,49 @@ mailsRouter.get("/future", async (req, res) => {
   res.status(200).json(filteredMails);
 });
 
+mailsRouter.put("/future/:id", async (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+
+  const mail = await Mail.findById(id);
+  console.log("mail: ", mail);
+  console.log("body: ", body);
+
+  const schedule = body.schedule;
+  let currentDate = new Date();
+  let nextDate;
+  console.log(schedule);
+  switch (schedule) {
+    case "recurring":
+      nextDate = new Date(
+        currentDate.setSeconds(currentDate.getSeconds() + 10)
+      );
+      break;
+    case "daily":
+      nextDate = new Date(currentDate.setDate(currentDate.getDate() + 1));
+      break;
+    case "weekly":
+      nextDate = new Date(currentDate.setDate(currentDate.getDate() + 7));
+      break;
+    case "monthly":
+      nextDate = new Date(currentDate.setDate(currentDate.getDate() + 30));
+      break;
+    case "yearly":
+      nextDate = new Date(currentDate.setDate(currentDate.getDate() + 365));
+      break;
+  }
+
+  const newMail = {
+    ...body,
+    nextDate,
+    prevDate: nextDate,
+  };
+  const updatedMail = await Mail.findByIdAndUpdate(id, newMail, {
+    new: true,
+  });
+  res.status(201).json(updatedMail);
+});
+
 mailsRouter.get("/past", async (req, res) => {
   const allMails = await Mail.find({});
   const currDate = new Date();
